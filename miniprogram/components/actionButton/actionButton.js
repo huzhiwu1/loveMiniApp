2293ee9d91db1c3685f09fa6fd800096e08c8068
huzhiwu <1627983849@qq.com>
2020-02-25 00:20:52 +0800

    首页样式完成

---
+++ miniprogram/components/actionButton/actionButton.js
@@ -0,0 +1,48 @@
+// components/actionButton/actionButton.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    num:{
+      type:Number,
+      value:0
+    },
+    type:{
+      type:String,
+      value:'date'
+    },
+    title:{
+      type:String,
+      value:'题目'
+    },
+    imgUrl:{
+      type:String,
+      value:'/images/question.png'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    animationData:{}
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    startAnimation(){
+      console.log("dong")
+      let animation = wx.createAnimation({
+        duration:1000,
+        timingFunction:'ease-in-out'
+      })
+      animation.scale(1.2,1.2).step().scale(1,1).step()
+      this.setData({
+        animationData:animation.export()
+      })
+    }
+  }
+})
---
+++ miniprogram/components/intimacyNum/intimacyNum.js
@@ -0,0 +1,26 @@
+// components/intimacyNum/intimacyNum.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    num:{
+      type:Number,
+      value:0
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
